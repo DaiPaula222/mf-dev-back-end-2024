85473411e5855e841ff7583e8fd1af2cb1795dfa
DaiPaula222 <139081871+DaiPaula222@users.noreply.github.com>
2024-10-21 21:53:30 -0300

    Configuraçaõ do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-back-end-2024/Controllers/VeiculosController.cs
@@ -0,0 +1,20 @@
+﻿using mf_dev_back_end_2024.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+namespace mf_dev_back_end_2024.Controllers
+{
+    public class VeiculosController:Controller
+    {
+      private readonly AppDbContext _context;
+            public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    public async Task<IActionResult> Index()
+    {
+        var dados = await _context.Veiculos.ToListAsync();
+        return View(dados);
+    }
+        }
+}
